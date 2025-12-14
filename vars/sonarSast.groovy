6f7a248de710294be16cb3d46dcafb42cbde7287
abdulmunimsaid82@gmail.com <abdulmunimsaid82@gmail.com>
2025-12-14 14:35:22 +0300

    fixing sonar command

--- vars/sonarSast.groovy
+++ vars/sonarSast.groovy
@@ -8,20 +8,17 @@ def call(Map params = [:]) {
         def projectName = params.projectName ?: env.JOB_NAME
 //        def timeoutMinutes = params.timeoutMinutes ?: 10
 
-        // Default Maven command (can be overridden)
-        def sonarCmd = params.command ?: "mvn clean verify sonar:sonar " +
-                "-Dsonar.projectKey=${projectKey} " +
-                "-Dsonar.projectName=\"${projectName}\" " +
-                "-Dsonar.host.url=${SONAR_HOST_URL} " +
-                "-Dsonar.login=${SONAR_AUTH_TOKEN}"
+//        // Default Maven command (can be overridden)
+//        def sonarCmd = params.command ?: "mvn clean verify sonar:sonar " +
+//                "-Dsonar.projectKey=${projectKey} " +
+//                "-Dsonar.projectName=\"${projectName}\" " +
+//                "-Dsonar.host.url=${SONAR_HOST_URL} " +
+//                "-Dsonar.login=${SONAR_AUTH_TOKEN}"
 
         withSonarQubeEnv(sonarServer) {
-            // Use the Maven tool configured in Jenkins
-            def mvnHome = tool 'maven-3.8.7'  // Make sure this name matches your Jenkins tool config
-            sh "${mvnHome}/bin/mvn clean verify"  // Optional: compile first
-
-            echo "Running SonarQube scan..."
-            sh "${mvnHome}/bin/${sonarCmd}"
+            sh params.command ?: "mvn sonar:sonar " +
+                    "-Dsonar.projectKey=${projectKey} " +
+                    "-Dsonar.projectName=\"${projectName}\" "
         }
 
 //        echo "Waiting for SonarQube Quality Gate..."
