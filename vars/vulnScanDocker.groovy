9afb94e7067ea9732721835d23fa30df79e48d83
abdulmunimsaid82@gmail.com <abdulmunimsaid82@gmail.com>
2025-12-16 14:34:41 +0300

    added dependencies scanning

--- vars/vulnScanDocker.groovy
+++ vars/vulnScanDocker.groovy
@@ -2,11 +2,12 @@
 def call(Map params = [:]) {
     try {
         echo "Stage: Vulnerability Scan - Docker (parallel)"
-        parallel(
-                "Dependency Scan": { sh params.dependencyCmd ?: "mvn dependency-check:check || true" },
-                "Trivy Scan": { sh params.trivyCmd ?: "bash trivy-docker-image-scan.sh || true" },
-                "OPA Conftest": { sh params.opaCmd ?: "docker run --rm -v \$(pwd):/project openpolicyagent/conftest test --policy opa-docker-security.rego Dockerfile || true" }
-        )
+//        parallel(
+//                "Dependency Scan": { sh params.dependencyCmd ?: "mvn dependency-check:check || true" },
+//                "Trivy Scan": { sh params.trivyCmd ?: "bash trivy-docker-image-scan.sh || true" },
+//                "OPA Conftest": { sh params.opaCmd ?: "docker run --rm -v \$(pwd):/project openpolicyagent/conftest test --policy opa-docker-security.rego Dockerfile || true" }
+//        )
+        sh params.dependencyCmd ?: "mvn dependency-check:check || true"
         return [success: true]
     } catch (e) {
         env.failedStage = "Vulnerability Scan - Docker"
