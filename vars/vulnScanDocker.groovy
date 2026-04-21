890a074bb333feabba4314343357fe58cb6007bb
Munim <lsaid@softnet.co.tz>
2026-04-21 15:42:59 +0300

    comment dependency scan

--- vars/vulnScanDocker.groovy
+++ vars/vulnScanDocker.groovy
@@ -4,14 +4,16 @@ def call(Map params = [:]) {
         echo "Stage: Vulnerability Scan - Docker (parallel)"
         parallel(
                 "Dependency Scan": {
-                    def mvnHome  = tool name: 'mave-3.9.15', type: 'maven'
-                    def javaHome = tool name: 'jdk21', type: 'jdk'
-                    withEnv(["PATH+MAVEN=${mvnHome}/bin", "PATH+JAVA=${javaHome}/bin", "JAVA_HOME=${javaHome}"]) {
-                        withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
-                            sh "mvn dependency-check:check -Dnvd.api.key=$NVD_API_KEY"
-                            // Note: use single quotes to avoid Groovy interpolation warning
-                        }
-                    }
+                    // def mvnHome  = tool name: 'mave-3.9.15', type: 'maven'
+                    // def javaHome = tool name: 'jdk21', type: 'jdk'
+                    // withEnv(["PATH+MAVEN=${mvnHome}/bin", "PATH+JAVA=${javaHome}/bin", "JAVA_HOME=${javaHome}"]) {
+                    //     withCredentials([string(credentialsId: 'nvd-api-key', variable: 'NVD_API_KEY')]) {
+                    //         sh "mvn dependency-check:check -Dnvd.api.key=$NVD_API_KEY"
+                    //         // Note: use single quotes to avoid Groovy interpolation warning
+                    //     }
+                    // }
+
+                    echo "Dependency Scan - Working on it to integrate"
                 },
                 "Trivy Scan - Base Image": {
                     sh params.trivyCmd ?: "bash trivy-docker-image-scan.sh"
